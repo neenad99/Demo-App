9e66f3d77d3f2a7582c03ceb3aea352e3cca650b
Pratik K. Shah <pratikcalling@gmail.com>
2021-10-24 02:31:09 +0530

    Fix registration field error + Clean-up

--- frontend/script/person.js
+++ frontend/script/person.js
@@ -7,18 +7,17 @@ function addPerson(){
                  type: "POST",
                  url: "http://localhost:3000/api/auth",
                  data: JSON.stringify({
-                                          "email": firstName,
-                                          "firstName": lastName,
-                                          "lastName": email
+                                          "firstName": firstName,
+                                          "lastName": lastName,
+                                          "email": email
                                       }),
                  contentType: "application/json; charset=utf-8",
                  crossDomain: true,
                  dataType: "json",
                  success: function (data, status, jqXHR) {
-                     alert("Person added successfully");// write success in " "
+                    alert("Person added successfully");
                  },
                  error: function (jqXHR, status) {
-                     // error handler
                      console.log(jqXHR);
                      alert('failed with status code:' + status.code);
                  }
@@ -33,18 +32,17 @@ function fetchPeople(){
                  crossDomain: true,
                  dataType: "json",
                  success: function (users, status, jqXHR) {
-                     var html = "";
+                     var tBody = "";
                      $.each(users.users,function(index,user){
-                        html+="<tr><td>"+user.firstName+"</td><td>"+user.lastName+"</td><td>"+user.email+"</td></tr>";
+                        tBody+="<tr><td>"+user.firstName+"</td><td>"+user.lastName+"</td><td>"+user.email+"</td></tr>";
                      });
 
                      $("#fetch_people_table thead").empty();
                      $("#fetch_people_table tbody").empty();
                      $("#fetch_people_table thead").append("<th>First Name</th><th>Last Name</th><th>Email</th>");
-                     $("#fetch_people_table tbody").append(html);
+                     $("#fetch_people_table tbody").append(tBody);
                  },
                  error: function (jqXHR, status) {
-                     // error handler
                      console.log(jqXHR);
                      alert('failed with status code:' + status.code);
                  }
