56c1c2598c844a5dd715aadbda7f2b5bd148892f
Neenad <neenadkambli@gmail.com>
2021-10-24 11:01:21 +0530

    served frontend files to backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const morgan = require("morgan");
+const path = require('path');
 
 const authRoute = require('./routes/authRoute');
 const userRoute = require('./routes/userRoute');
@@ -24,9 +25,15 @@ app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
+const static_path = path.join(__dirname, "../frontend");
 
 app.use('/api/auth',authRoute);
 app.use('/api/user',userRoute);
+app.use(express.static(static_path));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(static_path,'person.html'));
+});
 
 app.listen(port,()=>{
     console.log(`server started on ${port}`);
